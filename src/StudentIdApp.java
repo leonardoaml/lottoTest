476bb177480173da8c73a4d26de7081cb8c0c56c
x15007278 <x15007278@SLABS15-16-0007.student.nci.col>
2016-11-12 12:18:42 +0000

    first commit

---
+++ src/StudentIdApp.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author x15007278
+ */
+public class StudentIdApp {
+
+   String course;
+   String id;
+   int noQuest;//number of total question
+   int rightAns;//number of right questions
+   int result;
+    
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public int getNoQuest() {
+        return noQuest;
+    }
+
+    public void setNoQuest(int noQuest) {
+        this.noQuest = noQuest;
+    }
+
+    public int getRightAns() {
+        return rightAns;
+    }
+
+    public void setRightAns(int rightAns) {
+        this.rightAns = rightAns;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+    
+
+   
+
+   
+   
+   
+}
---
+++ src/ncirl/StudentId.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ncirl;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author x15007278
+ * Leonardo Amancio
+ */
+public class StudentId {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        StudentId studentIdApp;
+        studentIdApp = new StudentId();
+        
+        for (int i=0; i<i; i++){
+        course =String.parseStrg(JOptionPane.showInputDialog(null,"Please enter the course:"));
+        
+        if(course.equalsIgnoreCase('computing')){
+            noQuest = Integer.parseInt(JOptionPane.showInputDialog(null,"How many question overall?"));
+            rightAns = Integer.parseInt(JOptionPane.showInputDialog(null,"How many right answer?"));
+        
+        }
+        
+        else if(course.equalsIgnoreCase('business')){
+        noQuest = Integer.parseInt(JOptionPane.showInputDialog(null,"How many question overall?"));
+        rightAns = Integer.parseInt(JOptionPane.showInputDialog(null,"How many right answer?"));
+        
+        }
+        
+        
+        else if(course.equalsIgnoreCase('arts')){
+        noQuest = Integer.parseInt(JOptionPane.showInputDialog(null,"How many question overall?"));
+        rightAns = Integer.parseInt(JOptionPane.showInputDialog(null,"How many right answer?"));
+        
+        }
+        
+        
+        else {JOptionPane.showMessageDialog(null,"Ops, something went wrong")}
+                
+                
+                }
+     
+     }  
+        
+    
+        
+        // TODO code application logic here
+ //This chunck of code below it is just a sample of what I tryed out.       
+//    //variables
+//    int student;
+//    int noQuest;
+//    int noCorrect;
+//    int noWrong;
+//    int result = 0;
+//    
+//    
+//    
+//    //input
+//    student = Integer.parseInt(JOptionPane.showInputDialog(null,"Please, enter 1 for Computing| 2 for Business | 3 for Arts"));
+//    
+//    
+//    
+//    //process
+//    if (student ==1){
+//    noQuest = Integer.parseInt(JOptionPane.showInputDialog(null,"How many questions?"));
+//    noCorrect = Integer.parseInt(JOptionPane.showInputDialog(null,"How many right?"));
+//      
+//    }
+//    
+//    if (student == 2){
+//    noQuest = Integer.parseInt(JOptionPane.showInputDialog(null,"How many questions?"));
+//    noCorrect = Integer.parseInt(JOptionPane.showInputDialog(null,"How many right?"));
+//    
+//      
+//    
+//    }
+//    
+//    if (student ==3){
+//    noQuest = Integer.parseInt(JOptionPane.showInputDialog(null,"What is the total?"));
+//    result = noQuest;
+//        
+//    }
+//    
+//    
+//    //output
+//        
+//    JOptionPane.showMessageDialog(null," " + result);
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+    
+}
